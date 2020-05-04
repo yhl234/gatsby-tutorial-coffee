c88c317aa1b285e1f64494e3090207f73f7fb64d
yhl234 <200392342@student.georgianc.on.ca>
2020-05-04 18:45:07 -0400

    category filter

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import SEO from '../components/seo';
 import { Link, graphql } from 'gatsby';
 import BackgroundSection from '../components/Globals/BackgroundSection';
 import Info from '../components/Home/info';
+import Menu from '../components/Home/Menu';
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <BackgroundSection img={data.img.childImageSharp.fluid} title="Regular Joe's" styleClass="default-background" />
     <Info />
+    <Menu items={data.menu} />
   </Layout>
 );
 
@@ -18,6 +20,24 @@ export const query = graphql`
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          price
+          title
+          description {
+            description
+          }
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
